eddbafebaa9b35a073ff312b91bc34faa26df240
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2024-11-15 15:40:55 +0100

    feat: Add user data context

--- src/contexts/userData.Provider.tsx
+++ src/contexts/userData.Provider.tsx
@@ -1,8 +1,14 @@
 import { PropsWithChildren, useState } from "react";
 import { userDataContext } from "./userData";
+import { IUser } from "../types";
 
 export function UserDataProvider({ children }: PropsWithChildren) {
-  const [data, setData] = useState({ email: "", verified: false });
+  const [data, setData] = useState<IUser>({
+    email: "",
+    _id: "",
+    role: "client",
+    verified: false,
+  });
 
   return (
     <userDataContext.Provider value={{ data, setData }}>
