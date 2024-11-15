634d53d5b1200902c1ead33fcc6006156756bae0
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2024-11-15 15:25:51 +0100

    WIP: Add email service

---
+++ server/app/services/email/index.ts
@@ -0,0 +1,32 @@
+import { createTransport } from "nodemailer";
+
+const { SMTP_SERVER, EMAIL_ADDRESS, EMAIL_PASSWORD, ADMIN_NAME, BASE_URL } =
+  process.env;
+
+const transporter = createTransport({
+  host: SMTP_SERVER,
+  port: 465,
+  secure: true, // true for port 465, false for other ports
+  auth: {
+    user: EMAIL_ADDRESS,
+    pass: EMAIL_PASSWORD,
+  },
+});
+
+async function sendEmail(receiver: string, subject: string, content: string) {
+  const info = await transporter.sendMail({
+    from: `"${ADMIN_NAME}" <${EMAIL_ADDRESS}>`, // sender address
+    to: receiver, // list of receivers
+    subject, // Subject line
+    // html: "<b>Hello world?</b>", // html body
+    html: content,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  return info;
+}
+
+export async function sendVerificationMail(receiver: string, token: string) {
+  const template = `Please click this link to verify your email for tracking banners ${BASE_URL}/auth/verify/${token}`;
+  await sendEmail(receiver, "Verify You Email", template);
+}
