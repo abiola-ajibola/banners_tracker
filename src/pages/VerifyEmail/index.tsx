7d7478c92fbb60146279da1ebc432e6692b90eac
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2024-11-15 15:45:25 +0100

    feat: do not use OTP for email verification

--- src/pages/VerifyEmail/index.tsx
+++ src/pages/VerifyEmail/index.tsx
@@ -1,5 +1,3 @@
-import { OTPInputGroup } from "../../components/InputOTP";
-
 export function VerifyEmail() {
   return (
     <div
@@ -7,7 +5,8 @@ export function VerifyEmail() {
       className="h-[calc(100%-150px)] flex items-center w-full"
     >
       <div className="m-auto">
-        <OTPInputGroup onComplete={console.log} />
+        <p className="w-full">A verification email has been sent to you email address</p>
+        <p className="w-full">Please verify your email to continue</p>
       </div>
     </div>
   );
