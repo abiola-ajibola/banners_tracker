dec19ccc18696694d5bbe9dc0855f923d35eca6e
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2024-11-15 15:39:16 +0100

    feat: Add api handlers

---
+++ src/lib/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { IUser, MapLocation } from "../types";
+
+const api = axios.create({
+  baseURL: "/",
+});
+
+export async function auth(payload: { email: string; reset?: boolean }) {
+  const { data, status } = await api.post<IUser>("/auth", payload);
+  return { data, status };
+}
+
+export async function getMe() {
+  const { data, status } = await api.get<IUser>("/me");
+  return { data, status };
+}
+
+export async function saveLocation(location: MapLocation & { email: string }) {
+  const { data, status } = await api.put<MapLocation & { email: string }>(
+    "/banner-location",
+    location
+  );
+  return { data, status };
+}
